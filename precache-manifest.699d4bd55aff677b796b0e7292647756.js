3f07b1f6de6843ecb9b5c1f4ff0597f22d6e4972
kaigor619 <38076486+kaigor619@users.noreply.github.com>
2020-08-31 23:47:08 +0300

    Updates

---
+++ precache-manifest.699d4bd55aff677b796b0e7292647756.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "223b2919f953c5ccca5257366597c677",
+    "url": "/weather/index.html"
+  },
+  {
+    "revision": "2bc2d4e8f4f616b2f201",
+    "url": "/weather/static/css/main.b0b7a2d0.chunk.css"
+  },
+  {
+    "revision": "258bbd03b0ca3fdf4239",
+    "url": "/weather/static/js/2.2597b538.chunk.js"
+  },
+  {
+    "revision": "ec1add61b541a6106f0424d88e93f613",
+    "url": "/weather/static/js/2.2597b538.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2bc2d4e8f4f616b2f201",
+    "url": "/weather/static/js/main.06c66c94.chunk.js"
+  },
+  {
+    "revision": "65a135568c88d392bf25",
+    "url": "/weather/static/js/runtime-main.d8725fa2.js"
+  }
+]);
